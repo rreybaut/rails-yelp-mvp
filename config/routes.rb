c71966b8d573e0f749cab4f22387fb5d83dc9bfd
raphael reybaut <rreybaut@gmail.com>
2022-11-16 17:09:16 +0100

    review à terminer

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  resources :restaurants, only: [:index, :new, :show, :create] do
+    resources :reviews, only: [:new, :create]
+  end
   # Defines the root path route ("/")
   # root "articles#index"
 end
